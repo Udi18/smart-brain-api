8b1425d4f4310cc44591e85b21e516d28b1fe75a
Udi Levy <webdevudi@gmail.com>
2018-11-12 13:45:59 -0600

    Changed blah

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send('blah') })
+app.get('/', (req, res) => { res.send('Testing, testing. It works!') })
 app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', register.handleRegister(db, bcrypt));
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db) });
