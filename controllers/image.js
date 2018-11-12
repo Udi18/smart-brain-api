7427467dda9850ea1b8412b9c59db54b57e90235
Udi Levy <webdevudi@gmail.com>
2018-11-12 09:32:22 -0600

    First Commit

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '8eb7d53a13f34547bc9306fb2fa16a6d'
+   });
+
+const handleApiCall = (req, res) =>{
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage,
+    handleApiCall
+}
